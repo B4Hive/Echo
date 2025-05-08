45c803be77c188898d51287f0e8b1643c4a7a114
B4Hive <b4.bruno.santos@gmail.com>
2025-05-08 02:40:07 -0300

    Refactor EchoClient and EchoServer to "try-with-resources"

--- EchoClient/src/main/java/br/ufjf/b4hive/EchoClient.java
+++ EchoClient/src/main/java/br/ufjf/b4hive/EchoClient.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class EchoClient {
     public static void main(String[] args) throws Exception {
-        // verifica se o usuário digitou os parâmetros corretos
         if (args.length != 1) {
             System.err.println("ERROR: java EchoClient <host>");
             System.exit(1);
@@ -16,31 +15,28 @@ public class EchoClient {
         String host = args[0];
         int port = 4444;
 
-        // conecta ao servidor e abre os streams
-        Socket socket = new Socket(host, port);
-        Scanner stdin = new Scanner(System.in);
-        Scanner in = new Scanner(socket.getInputStream());
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        System.err.println("Connected to " + host + " on port " + port + "\nType 'exit' to close the connection");
-
-        System.err.print("[CLIENT]: ");
-        while (stdin.hasNextLine()) {
-            String s = stdin.nextLine();
-            if (s.equalsIgnoreCase("exit")) {
-                out.println(s);
-                break;
+        try (Socket socket = new Socket(host, port)) {
+            Scanner in;
+            try (Scanner stdin = new Scanner(System.in)) {
+                in = new Scanner(socket.getInputStream());
+                try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+                    System.err.println("Connected to " + host + " on port " + port + "\nType 'exit' to close the connection");
+                    System.err.print("[CLIENT]: ");
+                    while (stdin.hasNextLine()) {
+                        String s = stdin.nextLine();
+                        if (s.equalsIgnoreCase("exit")) {
+                            out.println(s);
+                            break;
+                        }
+                        out.println(s);
+                        String response = in.nextLine();
+                        System.out.println("[" + screenName + "]: " + response);
+                        System.err.print("[CLIENT]: ");
+                    }
+                    System.err.println("Closing connection to " + host);
+                }
             }
-            out.println(s);
-            String response = in.nextLine();
-            System.out.println("[" + screenName + "]: " + response);
-            System.err.print("[CLIENT]: ");
+            in.close();
         }
-
-        // encerra os sockets
-        System.err.println("Closing connection to " + host);
-        out.close();
-        stdin.close();
-        in.close();
-        socket.close();
     }
 }
--- EchoServer/src/main/java/br/ufjf/b4hive/EchoServer.java
+++ EchoServer/src/main/java/br/ufjf/b4hive/EchoServer.java
@@ -11,36 +11,29 @@ public class EchoServer {
         InetAddress ipAddress = InetAddress.getLocalHost();
         String ip = ipAddress.getHostAddress();
         int port = 4444;
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.err.println("Started server on " + ip +":"+ port + "\nPress CTRL+C to stop the server");
-
-        boolean running = true;
-        while (running) {
-            // espera blocante até alguma requisição de conexão
-            Socket clientSocket = serverSocket.accept();
-            System.err.println("Accepted connection from client");
-
-            // Cria as “streams” para o socket (buffer)
-            Scanner in = new Scanner(clientSocket.getInputStream());
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            // espera a leitura do dado (até terminar conexão)
-            String s;
-            while (in.hasNextLine()) {
-                s = in.nextLine().trim();
-                // Se o cliente enviar "exit", fecha a conexão
-                if (s.equalsIgnoreCase("exit")) {
-                    running = false;
-                    break;
-                } else {
-                    out.println(s.toUpperCase());
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.err.println("Started server on " + ip + ":" + port + "\nPress CTRL+C to stop the server");
+            boolean running = true;
+            while (running) {
+                Socket clientSocket = serverSocket.accept();
+                System.err.println("Accepted connection from client");
+                try (Scanner in = new Scanner(clientSocket.getInputStream())) {
+                    PrintWriter out;
+                    out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    String s;
+                    while (in.hasNextLine()) {
+                        s = in.nextLine().trim();
+                        if (s.equalsIgnoreCase("exit")) {
+                            running = false;
+                            break;
+                        } else {
+                            out.println(s.toUpperCase());
+                        }
+                    }
+                    System.err.println("Closing connection with client");
+                    out.close();
                 }
             }
-
-            // Fecha a conexão (e o socket)
-            System.err.println("Closing connection with client");
-            out.close();
-            in.close();
         }
         System.err.println("Closing server");
     }
