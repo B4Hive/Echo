9c414b1a3924e12e5e33b0e92182f6a925733e31
B4Hive <b4.bruno.santos@gmail.com>
2025-05-22 13:22:43 -0300

    Initialized implementation of echo command on EchoClient and EchoServer
    i'll finish it later

--- EchoClient/src/main/java/br/ufjf/b4hive/EchoClient.java
+++ EchoClient/src/main/java/br/ufjf/b4hive/EchoClient.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 
 public class EchoClient {
 
-    @SuppressWarnings({"resource", "ConvertToTryWithResources"})
+    @SuppressWarnings({"ConvertToTryWithResources"})
     public static void main(String[] args) throws Exception {
+
         if (args.length != 1) {
             System.out.println("ERROR: java EchoClient.java <host>");
             //System.exit(1);
@@ -17,28 +18,27 @@ public class EchoClient {
             scanner.close();
         }
 
-        String screenName = args[0];
         String host = args[0];
         int port = 4444;
-
         Socket socket = new Socket(host, port);
-        Scanner stdin = new Scanner(System.in);
-        Scanner in = new Scanner(socket.getInputStream());
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        System.err.println("Connected to " + host + " on port " + port + "\nType 'exit' to close the connection");
-        System.err.print("[CLIENT]: ");
-        while (stdin.hasNextLine()) {
-            String s = stdin.nextLine();
-            if (s.equalsIgnoreCase("exit")) {
-                out.println(s);
-                break;
-            }
-            out.println(s);
-            String response = in.nextLine();
-            System.out.println("[" + screenName + "]: " + response);
-            System.err.print("[CLIENT]: ");
-            in.close();
+        Scanner scanner = new Scanner(System.in);
+        Scanner incoming = new Scanner(socket.getInputStream());
+        PrintWriter outgoing = new PrintWriter(socket.getOutputStream(), true);
+
+        System.out.println("Connected to " + host + " on port " + port + "\nType 'quit' to close the connection");
+        System.out.print("[CLIENT]: ");
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            outgoing.println("[" + host + "]: " + s);
+            if (s.equalsIgnoreCase("quit")) break;
+            String response = incoming.nextLine();
+            System.out.println("[SERVER:" + host + "]: " + response);
+            System.out.print("[CLIENT]: ");
         }
-        System.err.println("Closing connection to " + host);
+        incoming.close();
+        scanner.close();
+        outgoing.close();
+        socket.close();
+        System.out.println("Closing connection to " + host);
     }
 }
--- EchoServer/src/main/java/br/ufjf/b4hive/EchoServer.java
+++ EchoServer/src/main/java/br/ufjf/b4hive/EchoServer.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 
 @SuppressWarnings("ConvertToTryWithResources")
 public class EchoServer {
+
 	public static void main(String[] args) throws Exception {
         InetAddress ipAddress = InetAddress.getLocalHost();
         String ip = ipAddress.getHostAddress();
@@ -16,14 +17,14 @@ public class EchoServer {
         @SuppressWarnings("resource")
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Started server on " + ip + ":" + port);
-		System.out.println("Type 'exit' to stop the server.");
+		System.out.println("Type 'quit' to stop the server.");
 
 		Thread commandThread = new Thread(() -> handleCommands());
 		commandThread.start();
         boolean running = true;
         while (running) {
             Socket clientSocket = serverSocket.accept();
-            System.err.println("Accepted connection from client");
+            System.out.println("Accepted connection from client");
 
             Thread thread = new Thread(() -> handleClient(clientSocket));
             thread.start();
@@ -32,16 +33,26 @@ public class EchoServer {
 
     private static void handleClient(Socket clientSocket) {
         try {
-            Scanner in = new Scanner(clientSocket.getInputStream());
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            Scanner incoming = new Scanner(clientSocket.getInputStream());
+            PrintWriter outgoing = new PrintWriter(clientSocket.getOutputStream(), true);
             String s;
-            while (in.hasNextLine()) {
-                s = in.nextLine().trim();
-                out.println(s.toUpperCase());
+            while (incoming.hasNextLine()) {
+                s = incoming.nextLine().trim();
+                String[] parts = s.split("]:");
+                s = parts[1].trim();
+                if (s.equalsIgnoreCase("quit")) {
+                    System.out.println("Client disconnected");
+                    break;
+                } else {
+                    if(s.length() > 5 && s.substring(0, 5).equalsIgnoreCase("echo "))
+                        outgoing.println(s.substring(5).toUpperCase());
+                    else //adiciona tratamento caso a mensagem inteira seja "echo" sem nada depois
+                        outgoing.println("ERROR: Unknown command");
+                }
             }
-            System.err.println("Closing connection with client");
-            in.close();
-            out.close();
+            System.out.println("Closing connection with client");
+            incoming.close();
+            outgoing.close();
             clientSocket.close();
         } catch (IOException ex) {
             System.err.println("Error: " + ex.getMessage());
@@ -52,12 +63,12 @@ public class EchoServer {
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
 			String command = scanner.nextLine();
-			if (command.equalsIgnoreCase("exit")) {
+			if (command.equalsIgnoreCase("quit")) {
 				System.out.println("Stopping server...");
 				System.exit(0);
 				break;
 			} else {
-				System.out.println("Unknown command: " + command);
+				System.err.println("Unknown command: " + command);
 			}
 		}
 		scanner.close();
